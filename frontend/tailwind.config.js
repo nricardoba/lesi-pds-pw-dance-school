f6205ffef09eb2b5201eb6a757d323082eeaefa0
Ricardo Araújo <73717911+nricardoba@users.noreply.github.com>
2026-04-05 00:12:40 +0100

    Add Vite frontend boilerplate

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  // Diz ao Tailwind para analisar todos os ficheiros nestas pastas
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      colors: {},
+      colors: {
+        brand: {},
+      },
       fontFamily: {},
     },
   },
