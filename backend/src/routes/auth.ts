770d5feb3de4efa4ff4df821f0cd295a517f97a3
Leonel Diogo Ribeiro Freitas <a31520@alunos.ipca.pt>
2026-04-30 12:07:09 +0100

    Done verify role when checking the token if its still the same and user now gets quicked out after token gets expired

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,10 +1,12 @@
 ﻿import { Router } from 'express';
 import * as AuthController from '../controllers/authController';
+import { ensureAuth } from '../middlewares/ensureAuth';
 
 const router = Router();
 
 router.post('/login', AuthController.loginController);
 router.post('/register', AuthController.registerController);
 router.post('/setup-credentials', AuthController.setupCredentialsController);
+router.get('/verify', ensureAuth, AuthController.verifyTokenController);
 
 export default router;
