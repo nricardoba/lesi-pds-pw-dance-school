56f76e9a6383fdbd85868bd74714f57e52337e53
Pedro Sousa <125359374+Pedrof13@users.noreply.github.com>
2026-06-04 16:26:19 +0100

    Frontend: update CORS configuration to allow requests from the production URL

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,7 +13,7 @@ const port = process.env.PORT || 3333;
 
 // Middlewares
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+  origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://lesi-pds-pw-dance-school.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
